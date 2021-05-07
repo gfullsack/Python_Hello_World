6ce66bfc2f0fa2790213dfd3febf473ffa4315c5
Gerard Fa Fullsack <gfullsack@gmail.com>
2021-05-07 11:34:18 -0700

    Create hello_world.py
    First Commit of Homework Assignment

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+# 1. TASK: print "Hello World"
+print("Hello World")
+# 2. print "Hello Noelle!" with the name in a variable
+name = "Gerard"
+print("Hello", name)
+print("Hello " + name)
+
+# 3. print "Hello 42!" with the number in a variable
+num = "88"
+print("Hello", num)
+print("Hello " + num)
+
+# 4. print "I love to eat sushi and pizza." with the foods in variables
+food_one = "Cheesecake"
+food_two = "Milk Tea"
+
+print(f"I love to eat", food_one, "and", food_two,)
+print(f"My name is", name, " and I love to eat", food_one, "and", food_two, " and my favorite number is", num,)
